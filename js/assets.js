4a42875524d5ac781779df2827408287e8b245fc
רעות צפלביץ <tzipilev@g.jct.ac.il>
2024-02-12 09:10:25 +0200

    cookies

--- js/assets.js
+++ js/assets.js
@@ -1,15 +1,15 @@
-
 function handleLogin(user) {
     localStorage.setItem('currentUser',  JSON.stringify(user));
-    localStorage.setItem('isLoggedIn', 'true');
+    //localStorage.setItem('isLoggedIn', 'true');
+    setCookie("user", JSON.stringify(user), 1);
     window.parent.location.href = '../index.html';
 }
 
 function handleLogout() {
-    // Clear session storage and redirect to login page
     localStorage.removeItem('currentUser');
-    localStorage.setItem('isLoggedIn', 'false');
+    //localStorage.setItem('isLoggedIn', 'false');
     localStorage.setItem('update', 'false');
+    deleteCookie("user");
     window.parent.location.href = '../index.html';
 }
 
@@ -18,33 +18,35 @@ function showAlert() {
 }
 
 function addScore(fileName, userName, score) {
-    let scores = JSON.parse(localStorage.getItem(fileName)) || [];
-
-    fetch(`/json/${fileName}.json`)
+  fetch(`/json/${fileName}.json`)
     .then((response) => response.json())
     .then((data) => {
-        const existingUserIndex = data.findIndex(item => item.userName === userName);
-        
-        if (existingUserIndex !== -1) {
-            // User already exists in data
-            const existingScore = parseInt(data[existingUserIndex].score);
-            
-            if (score > existingScore) {
-                // New score is greater, replace the existing score
-                data[existingUserIndex].score = `${score}`;
-            }
-        } else {
-            // User doesn't exist, add a new entry
-            const userScore = {
-                userName: userName,
-                score: `${score}`,
-            };
-            data.push(userScore);
+      const existingUserIndex = data.findIndex(
+        (item) => item.userName === userName
+      );
+
+      if (existingUserIndex !== -1) {
+        // User already exists in data
+        const existingScore = parseInt(data[existingUserIndex].score);
+
+        if (score > existingScore) {
+          // New score is greater, replace the existing score
+          data[existingUserIndex].score = `${score}`;
         }
+      } else {
+        // User doesn't exist, add a new entry
+        const userScore = {
+          userName: userName,
+          score: `${score}`,
+        };
+        data.push(userScore);
+      }
 
-        const updatedJSON = JSON.stringify(data);
+      const updatedJSON = JSON.stringify(data);
 
-         // Update localStorage
-         localStorage.setItem(fileName, updatedJSON);
+      // Update localStorage
+      localStorage.setItem(fileName, updatedJSON);
     });
 }
+
+
